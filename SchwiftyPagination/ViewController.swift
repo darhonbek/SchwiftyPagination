dfe7f17f80e63680886ddf954364c72cd52310b6
Darkhonbek Mamataliev <darkhonbek@gmail.com>
2020-10-25 00:13:42 -0400

    Initial Commit

---
+++ SchwiftyPagination/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SchwiftyPagination
+//
+//  Created by Darkhonbek Mamataliev on 10/25/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
